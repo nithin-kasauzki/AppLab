2b4999191f4d415ea005950f9d2b9c9b85215b48
nithin-kasauzki <106121086@nitt.edu>
2024-04-06 22:49:10 +0530

    fixed code

--- assignment/e.js
+++ assignment/e.js
@@ -1,10 +1,10 @@
-// Function to check if a number is prime
+// part i) Function to check if a number is prime
 function isPrime(num) {
     if (num <= 1) {
         return false;
     }
     
-    for (let i = 2; i <= Math.sqrt(num); i++) {
+    for (let i = 2; i <= (num)/2; i++) {
         if (num % i === 0) {
             return false;
         }
@@ -13,13 +13,15 @@ function isPrime(num) {
     return true;
 }
 
-// Find and print all prime numbers between 1 and 10,000
+// part ii)Find and print all prime numbers between 1 and 10,000
 let primes = [];
 for (let i = 1; i <= 10000; i++) {
     if (isPrime(i)) {
         primes.push(i);
     }
 }
+let textareaResult = document.getElementById("result");
+textareaResult.value += "Prime Numbers: " + primes.join(", ");
 
 
 
